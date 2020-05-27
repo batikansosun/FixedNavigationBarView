4a15deac90cd9abbebd29abdd25b92882be5784c
Batıkan Sosun <batikansosun@gmail.com>
2020-05-27 13:09:52 +0300

    Update test_app.sh

--- .github/test_app.sh
+++ .github/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -project FixedNavigationBarView.xcworkspace \
+xcodebuild -project FixedNavigationBarView.xcodeproj \
             -scheme FixedNavigationBarView\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
